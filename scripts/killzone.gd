f00a9431b3c530f446cfe6d36d88871cc11dc57f
afan104 <101306676+afan104@users.noreply.github.com>
2024-06-12 18:08:20 -0700

    Update killzone.gd

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,15 +2,16 @@ extends Area2D
 
 
 @onready var timer = $Timer
-
+var slowed_time_scale = 0.5
+var default_time_scale = 1
 
 func _on_body_entered(body):
 	print("You died...")
-	Engine.time_scale = 0.5
+	Engine.time_scale = slowed_time_scale
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 
 func _on_timer_timeout():
-	Engine.time_scale = 1
+	Engine.time_scale = default_time_scale
 	get_tree().reload_current_scene()
